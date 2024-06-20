874b7055a707cca7e69dd6937773cee51e183a06
Yangx <641160771@qq.com>
2024-06-20 15:29:52 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,7 +36,9 @@ const loading = ref(false);
 
 let lastClaimTime = ref(0);
 const amount = '10';
-const token = ref('');
+const token = ref(
+  '0.Qu2Aqn7BdnAfFOaSy9v_1a7e9zQ2uh7KUaxblpyMOQZcQyXGyKsXI23ha2z9MFOavKdEWRnIzdpf5izabmSi3KR8gM65_V-AS_GfPQjjqPnw1AzfxItEk20jgG5725GalYwShVKRDQ5-L2PBCxMKxMcGlO-_WufpYyyE66kJPI7mCpjhJWPRAQpOsJgjelEyv8b8600QG3diNmVn0zg9u1p3nHbM5T56AVJsxIxBjxx_WcATJVrNkiGgBNFKrNmtcQWLK3nFUp-KBON74dBS4RIT1GUuztNjvFsU2rw2ToSLh90VehoURWQY9TjTsdtaOOjGE5BGwZ8yYczNIXHJqZ2ttuhIlNG5yfg28l_NZRTzFogCqhNpDhb7jFG-xaPw0V9AA6MlZMTK_2owTicv22_jFv8vBrp1E0fXRV0wdgngfa6bv0Vxc7Pmmz2tbti8.2WnkrOivdigua5ttFQT74A.7a06cc29ff2e8aad52fabde3268111b30af781f4e2bde1781fe1d4e378fca740'
+);
 
 const rpc = 'https://rpc.hypergrid.dev';
 const explorer = 'https://explorer.hypergrid.dev/tx/';
@@ -57,13 +59,14 @@ const handleClaim = () => {
   if (timeDiff >= 5 * 60 * 1000) {
     lastClaimTime.value = currentTime;
     loading.value = true;
-
     apis
       .getAirdrop(addressVal.value, amount, token.value)
       .then((res: any) => {
-        // console.log('getAirdrop', res);
+        console.log('getAirdrop', res.data);
         loading.value = false;
+
         if (res.data.data) {
+          if (res.data.data.error) return message.error(res.data.data.error);
           localStorage.setItem('lastClaimTime', lastClaimTime.value.toString());
           const tx = res.data.data.replace(/\n/g, '').replace('Signature: ', '');
           console.log('tx', tx);
@@ -79,7 +82,6 @@ const handleClaim = () => {
             duration: null
           });
         } else {
-          // console.log('res.data.err', res.data.err);
           if (
             res.data.err == "error: Invalid value for '<RECIPIENT_ADDRESS>': No such file or directory (os error 2)\n"
           ) {
